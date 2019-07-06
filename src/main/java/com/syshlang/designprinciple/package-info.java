c6abc4692cf40ae8e97d933d64e4f08d3f9750b7
syshlang <158990605@qq.com>
2019-07-06 23:04:55 +0800

    Initial commit

---
+++ src/main/java/com/syshlang/designprinciple/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 设计模式的七大原则:
+ * 单一职责原则 (Single Responsibility Principle)
+ * 开闭原则（Open Close Principle）
+ * 里氏代换原则（Liskov Substitution Principle）
+ * 依赖倒转原则（Dependence Inversion Principle）
+ * 接口隔离原则（Interface Segregation Principle）
+ * 迪米特法则（最少知道原则）（Demeter Principle）
+ * 合成复用原则（Composite Reuse Principle）
+ */
+package com.syshlang.designprinciple;
