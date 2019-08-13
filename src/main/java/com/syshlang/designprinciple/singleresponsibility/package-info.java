af3ebefdfce52fc0c265a2de2a415a996a2cd063
syshlang <sunyslang@gmail.com>
2019-08-13 17:06:17 +0800

    feat(<b>设计模式的七大原则:</b>): 单一职责原则 (Single Responsibility Principle)
    单一职责原则 (Single Responsibility Principle)

---
+++ src/main/java/com/syshlang/designprinciple/singleresponsibility/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <b>设计模式的七大原则:</b><br>
+ * 单一职责原则 (Single Responsibility Principle)
+ * 对于一个类而言，不应存在多于一个导致类变更的原因，也就是说一个类只负责一项职责。
+ */
+package com.syshlang.designprinciple.singleresponsibility;
