29b9a943ffd62ba3111a51b52de9ed2f7d34067e
syshlang <sunyslang@gmail.com>
2019-08-19 16:13:15 +0800

    Interface Segregation Principle

---
+++ src/main/java/com/syshlang/designprinciple/interfacesegregation/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <b>设计模式的七大原则:</b><br>
+ * 接口隔离原则 (Interface Segregation Principle)
+ * 客户端不应该依赖它不需要的接口，即一个类对另一个类的依赖应该建立在最小的接口上
+ */
+package com.syshlang.designprinciple.interfacesegregation;
