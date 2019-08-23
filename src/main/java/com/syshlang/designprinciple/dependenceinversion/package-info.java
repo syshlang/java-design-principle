62d9d626b5870ff0feca539bddd38c18ca1c4c43
syshlang <sunyslang@gmail.com>
2019-08-23 11:50:08 +0800

    Dependence Inversion Principle

---
+++ src/main/java/com/syshlang/designprinciple/dependenceinversion/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <b>设计模式的七大原则:</b><br>
+ * 依赖倒转(倒置)原则(DIP: Dependence Inversion Principle)
+ * 三层含义:
+ * - 高层模块不应该依赖低层模块，两者都应该依赖其抽象；
+ * - 抽象不应该依赖细节；
+ * - 细节应该依赖抽象。
+ */
+package com.syshlang.designprinciple.dependenceinversion;
