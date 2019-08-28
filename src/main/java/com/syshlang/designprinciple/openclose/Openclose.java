7bfed3c246222e73815a0470b0f832e3c0d5eb16
syshlang <sunyslang@gmail.com>
2019-08-28 14:18:15 +0800

    Open Close Principle

---
+++ src/main/java/com/syshlang/designprinciple/openclose/Openclose.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019.
+ * @File: Openclose.java
+ * @Description:
+ * @Author: sunys
+ * @Date: 2019/8/28 上午11:36
+ * @since:
+ */
+
+package com.syshlang.designprinciple.openclose;
+
+/**
+ * @author sunys
+ */
+public class Openclose {
+
+}
---
+++ src/main/java/com/syshlang/designprinciple/openclose/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <b>设计模式的七大原则:</b><br>
+ * 开闭原则 (Open Close Principle)
+ *编程中最基础、最重要的设计原则。它指出，一个软件中的对象（类，模块，函数等等）应该对于扩展是开放的(对提供方)，
+ * 但是对于修改是封闭的(对使用方)。
+ */
+package com.syshlang.designprinciple.openclose;
