a95b00222abec878baa7daaddf255b0ed434664f
syshlang <158990605@qq.com>
2019-09-02 23:13:51 +0800

    Composite Reuse Principle

---
+++ src/main/java/com/syshlang/designprinciple/compositereuse/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <b>设计模式的七大原则:</b><br>
+ * 合成复用原则(CRP:Composite Reuse Principle)
+ * 又称为组合/聚合复用原则(CARP:Composition/Aggregate Reuse Principle,)，
+ * 该原则指出尽量使用合成/聚合，尽量不要使用类继承。
+ */
+package com.syshlang.designprinciple.compositereuse;
